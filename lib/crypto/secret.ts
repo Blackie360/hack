4d39b9efdade6b41215dec0fbe13164bea57be9a
blackie360 <felixkent360@gmail.com>
2025-10-15 20:27:52 +0300

    fix

--- lib/crypto/secret.ts
+++ lib/crypto/secret.ts
@@ -4,7 +4,7 @@ export async function encryptAesGcm(keyBytes: Uint8Array, plaintext: Uint8Array,
   const key = await crypto.subtle.importKey("raw", keyBytes as BufferSource, { name: "AES-GCM" }, false, ["encrypt"]);
   const iv = crypto.getRandomValues(new Uint8Array(12));
   const algorithmParams: AesGcmParams = aad 
-    ? { name: "AES-GCM", iv, additionalData: aad }
+    ? { name: "AES-GCM", iv, additionalData: aad as BufferSource }
     : { name: "AES-GCM", iv };
   const enc = await crypto.subtle.encrypt(
     algorithmParams,
@@ -17,7 +17,7 @@ export async function encryptAesGcm(keyBytes: Uint8Array, plaintext: Uint8Array,
 export async function decryptAesGcm(keyBytes: Uint8Array, iv: Uint8Array, ciphertext: Uint8Array, aad?: Uint8Array) {
   const key = await crypto.subtle.importKey("raw", keyBytes as BufferSource, { name: "AES-GCM" }, false, ["decrypt"]);
   const algorithmParams: AesGcmParams = aad 
-    ? { name: "AES-GCM", iv, additionalData: aad }
+    ? { name: "AES-GCM", iv, additionalData: aad as BufferSource }
     : { name: "AES-GCM", iv };
   const dec = await crypto.subtle.decrypt(
     algorithmParams,
